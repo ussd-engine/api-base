bded60b366ee218164f7c3ecd10a543664ef14c6
Stefan Froelich <sfroelich01@gmail.com>
2022-09-02 02:09:24 +0000

    streamline includes

--- lib/api_base/service.rb
+++ lib/api_base/service.rb
@@ -1,15 +1,7 @@
 # frozen_string_literal: true
 
-require 'faraday'
-require 'stoplight'
-require 'api_base/concerns/traceable'
-require 'api_base/concerns/filterer'
-
 module ApiBase
   module Service
-    include Concerns::Traceable
-    include Concerns::Filterer
-
     def service_name
       raise NotImplementedError, 'service_name is not implemented'
     end
